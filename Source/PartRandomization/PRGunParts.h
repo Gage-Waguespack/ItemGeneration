7e7272b58e1247251ee35d23a016e6686328c81f
Gage Waguespack <gagewaguespack@gmail.com>
2021-12-16 16:07:36 -0600

    Keep getting error, unsure where to go from here, will continue tomorrow

--- Source/PartRandomization/PRGunParts.h
+++ Source/PartRandomization/PRGunParts.h
@@ -36,6 +36,32 @@ public:
 };
 
 USTRUCT(BlueprintType)
+struct FPRBarrelsViewOnly
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		FString Name = "";
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		UMeshComponent* Mesh;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int Rarity = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int BulletCount = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int BulletSpread = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int DamageModifier = 0;
+};
+
+USTRUCT(BlueprintType)
 struct FPRUnderBarrels
 {
 	GENERATED_USTRUCT_BODY()
@@ -45,13 +71,33 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		FString Name = "";
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		UMeshComponent* Mesh;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		int Rarity = 0;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float ADSTime = 0;
+};
+
+USTRUCT(BlueprintType)
+struct FPRUnderBarrelsViewOnly
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		FString Name = "";
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		UMeshComponent* Mesh;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int Rarity = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
 		float ADSTime = 0;
 };
 
@@ -79,6 +125,29 @@ public:
 };
 
 USTRUCT(BlueprintType)
+struct FPRMagazinesViewOnly
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		FString Name = "";
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		UMeshComponent* Mesh;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int Rarity = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int AmmoConsumption = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int MagSize = 0;
+};
+
+USTRUCT(BlueprintType)
 struct FPRStocks
 {
 	GENERATED_USTRUCT_BODY()
@@ -98,18 +167,47 @@ public:
 		int RecoilReduction = 0;
 };
 
+USTRUCT(BlueprintType)
+struct FPRStocksViewOnly
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		FString Name = "";
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		UMeshComponent* Mesh;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int Rarity = 0;
+
+	UPROPERTY(VisableAnywhere, BlueprintReadOnly)
+		int RecoilReduction = 0;
+};
+
 UCLASS()
 class PARTRANDOMIZATION_API UPRGunParts : public UDataAsset
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Mags")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts")
 		TArray<FPRBarrels> Barrels;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Mags")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts")
 		TArray<FPRUnderBarrels> UnderBarrels;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Mags")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts")
 		TArray<FPRMagazines> Mags;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Mags")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts")
 		TArray<FPRStocks> Stocks;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts View Only")
+		TArray<FPRBarrelsViewOnly> BarrelsViewOnly;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts View Only")
+		TArray<FPRUnderBarrelsViewOnly> UnderBarrelsViewOnly;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts View Only")
+		TArray<FPRMagazinesViewOnly> MagsViewOnly;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "PR Gun Parts View Only")
+		TArray<FPRStocksViewOnly> StocksViewOnly;
 };
